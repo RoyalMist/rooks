0f818afd5d72bb3b503205c2873eedcff1d9b6f8
Thibault Fouache <thibault.fouache@gmail.com>
2025-02-12 11:15:11 +0100

    Refactor main.rs to update struct, plugin, and API config
    Renamed struct `Data` to `In` and added a new field, `school`, to handle extended input data. Modified the plugin configuration to include an allowed host and updated API call parameters for better clarity and compatibility. These changes enhance functionality and maintainability of the code.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ use extism::{Manifest, Plugin, Wasm};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Data {
+struct In {
     name: String,
     age: i32,
     happy: bool,
+    school: String,
 }
 
 fn main() {
-    let input = Data {
-        name: "John".to_string(),
+    let input = In {
+        name: "Thibault Fouache".to_string(),
         age: 30,
         happy: true,
+        school: "High One".to_string(),
     };
 
     let wasm_path = std::env::var("WASM_PATH").unwrap();
@@ -22,10 +24,13 @@ fn main() {
         meta: Default::default(),
     };
 
-    let manifest = Manifest::new([path]).with_config_key("redact", "xxxxx");
+    let manifest = Manifest::new([path])
+        .with_config_key("redact", "Francesco")
+        .with_allowed_host("api.chucknorris.io");
+
     let mut plugin = Plugin::new(&manifest, [], false).unwrap();
     let res = plugin
-        .call::<Json<Data>, Json<Data>>("redact", Json::from(input))
+        .call::<Json<In>, String>("call", Json::from(input))
         .unwrap();
 
     println!("{:?}", res);
